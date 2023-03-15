d5ae368dbae2322d74c4ca07ae6913e3bc317b9b
Susana Muñoz <su.munozh@gmail.com>
2023-03-15 20:28:48 -0300

    Primer Commit

---
+++ app/src/main/java/com/summhy/intent/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.summhy.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void anterior(View v){
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+
+    }
+}
